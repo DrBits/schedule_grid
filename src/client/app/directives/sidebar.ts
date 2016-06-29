cf5cb53dfc5780385e9bf9e2102ee82b1a2dde19
Alexey Levanov <ilewanof@gmail.com>
2016-06-29 12:43:15 +0400

    Panel patients is added

--- src/client/app/directives/sidebar.ts
+++ src/client/app/directives/sidebar.ts
@@ -2,7 +2,40 @@ import "angular"
 
 export default class Sidebar implements ng.IDirective {
 	public template = `
-			<div>
-				
-			</div>`
+			<div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+                        <li class="sidebar-item">
+                           	<div class="custom-info-search-form">
+															<span class="name-info">Пациенты</span>
+                                <span class="pull-right">
+                                	<div class="btn-group">
+  										<button class="btn btn-success btn-xs dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    										<i class="fa fa-user"></i> 
+    										<span class="caret"></span>
+  										</button>
+  										<ul class="dropdown-menu pull-left">
+    										<li>
+    											<a href="#"><i class="fa fa-power-off"></i> Завершить работу с пациентом</a>
+    										</li>
+  										</ul>
+									</div>
+                                </span>
+                                <div class="clearfix"></div>
+                            </div>
+                            <div class="input-group custom-search-form">
+                                <input type="text" class="form-control" placeholder="Search...">
+                                <span class="input-group-btn">
+                                <button class="btn btn-default" type="button">
+                                    <i class="fa fa-search"></i>
+                                </button>
+                            </span>
+                            </div>
+                            <!-- /input-group -->
+                        </li>
+                        <li class="sidebar-item">
+                        	
+                        </li>
+                    </ul>
+                </div>
+			`
 }
