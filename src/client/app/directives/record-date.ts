bff2fec683429573b77666926b6cded019e83400
Alexey Levanov <ilewanof@gmail.com>
2016-07-03 19:57:01 +0400

    added datepicker

--- src/client/app/directives/record-date.ts
+++ src/client/app/directives/record-date.ts
@@ -1,6 +1,15 @@
 import "angular";
 
+
+interface IRecordDateScope extends ng.IScope {
+    open2: () => void
+    popup2: boolean
+    format: string
+}
+
 export default class RecordDate implements ng.IDirective {
+    scope: IRecordDateScope
+    $scope: IRecordDateScope
 
     public template: string = `
 						<li class="sidebar-item">
@@ -9,13 +18,25 @@ export default class RecordDate implements ng.IDirective {
                         		<div class="clearfix"></div>
                         	</div>
                         	<div class="input-group custom-search-form date" id="datetimepicker1">
-                    			<input type='text' class="form-control" placeholder="ДД.ММ.ГГГГ"/>
+                    			<input type='text' class="form-control" uib-datepicker-popup={{format}} ng-model="dt" is-open="popup2" min-date="minDate" datepicker-options="dateOptions" ng-required="true" close-text="Close" placeholder="ДД.ММ.ГГГГ"/>
                     			<span class="input-group-btn">
-                        			<button class="btn btn-default">
+                        			<button class="btn btn-default" ng-click="open2()">
 										<i class="fa fa-calendar"></i>
                         			</button>
                     			</span>
             				</div>
                         </li>
 	`
+
+    private open2: () => void = () => {
+        this.$scope.popup2 = true
+    }
+
+    public link(scope: IRecordDateScope, element: ng.IAugmentedJQuery, attributes: ng.IAttributes) {
+        this.$scope = this.scope = scope
+        this.$scope.open2 = this.open2
+        this.$scope.format = "dd-MM-yyyy"
+
+    }
+
 }
